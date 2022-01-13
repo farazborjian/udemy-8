f57d6e2af6a11daab0690836baebf4abc59c18a8
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2022-01-13 14:02:14 -0800

    fixed

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,32 @@
 import React from 'react';
 
-const Table = ({tableData}) => {
-    
+const Table = ({ tableData }) => {
 
 
-    return (
+
+  return (
     <table border="1">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Zip Code</th>
-          </tr>
-          </thead>
-          <tbody>
-        {tableData.map((user, id) => <tr key={id}>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>{user.zipcode}</td>
-            </tr>)}
-        </tbody>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Zip Code</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((user, id) => {
+          return (
+            <tr key={id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.address.zipcode}</td>
+            </tr>
+          )
+        }
+        )}
+      </tbody>
     </table>
-    )
+  )
 }
 
 export default Table;
